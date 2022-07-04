831b0fd91ba7c37f74e1d314e193e9dd0c47d080
Will Nilges <wdn5796@rit.edu>
2022-07-04 13:38:26 -0400

    What I've got so far

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # proxstar-vnc-forwarder
 A proof-of-concept way to forward a VNC websocket from proxmox to an unauthenticated PVE user
+
+![image](https://user-images.githubusercontent.com/42927786/177199389-2fd2470a-468d-415d-91d2-5b6675f65deb.png)
---
+++ websocket_catcher.py
@@ -0,0 +1,95 @@
+from proxmoxer import ProxmoxAPI
+import json
+from http import client as httplib
+from selenium import webdriver
+import urllib.parse
+import requests
+
+import websocket
+
+def connect_proxmox():
+    for host in proxmox_hosts:
+        try:
+            proxmox = ProxmoxAPI(
+                host,
+                user=proxmox_user,
+                password=proxmox_pass,
+                verify_ssl=False,
+            )
+            proxmox.version.get()
+            return proxmox
+        except:
+            if proxmox_hosts.index(host) == (len(proxmox_hosts) - 1):
+                print('unable to connect to any of the given Proxmox servers')
+                raise
+
+def node(search_id):
+    proxmox = connect_proxmox()
+    for vm in proxmox.cluster.resources.get(type='vm'):
+        if vm['vmid'] == int(search_id):
+            return vm['node']
+    return None
+
+proxmox_hosts = ['proxmox03-nrh.csh.rit.edu','proxmox01-nrh.csh.rit.edu','proxmox02-nrh.csh.rit.edu']
+proxmox_user = 'username goes here@csh.rit.edu'
+proxmox_pass = 'password goes here'
+
+proxmox = connect_proxmox()
+
+node='proxmox01-nrh'
+burger_king_foot_lettuce=urllib.parse.quote_plus('&')
+search_id = 132
+
+def try_with_password():
+    data = {"username": proxmox_user, "password": proxmox_pass}
+    response_data = requests.post(
+        "https://proxmox01-nrh.csh.rit.edu:8006/" + "api2/json/access/ticket",
+        verify=False,
+        timeout=5,
+        data=data,
+    ).json()["data"]
+    if response_data is None:
+        raise AuthenticationError(
+            "Could not authenticate against `ticket` endpoint! Check uname/password"
+        )
+
+    print(f"response data (tell me ur secrets) {response_data}")
+
+    csrf_prevention_token = response_data['CSRFPreventionToken']
+    webbed_csrf_prevention_token = urllib.parse.quote_plus(csrf_prevention_token)
+    
+    ticket = response_data['ticket']
+    webbed_ticket = urllib.parse.quote(ticket)
+
+    proxy_params = {"node": node, "vmid": str(search_id), "websocket": '1', "generate-password": '1'}
+
+    vncproxy_response_data = requests.post(
+        "https://proxmox01-nrh.csh.rit.edu:8006" + f"/api2/json/nodes/{node}/qemu/{search_id}/vncproxy",
+        verify=False,
+        timeout=5,
+        params=proxy_params,
+        headers={"CSRFPreventionToken": csrf_prevention_token},
+        cookies={"PVEAuthCookie": ticket}
+    ).json()["data"]
+    if response_data is None:
+        raise AuthenticationError(
+            "Could not authenticate against `vncproxy` endpoint!"
+        )
+
+    print(vncproxy_response_data)
+
+    vnc_ticket = vncproxy_response_data['ticket']
+    vnc_port = vncproxy_response_data['port']
+    vnc_password = vncproxy_response_data['password']
+    webbed_vnc_ticket=urllib.parse.quote_plus(vnc_ticket)
+
+    ws = websocket.WebSocketApp(f'wss://proxmox01-nrh.csh.rit.edu/api2/json/nodes/{node}/qemu/{search_id}/vncwebsocket?vncticket={webbed_vnc_ticket}&port={vnc_port}',
+                        #   on_message = print("message"),
+                        #   on_error = print("fuck."),
+                        #   on_close = print("close"),
+                          on_data = print("got data"),
+                          cookie = f"PVEAuthCookie={ticket};"
+    )
+    ws.run_forever()
+
+try_with_password()
